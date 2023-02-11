404b7a19845e62c7fd1c7ffb0456f3029c00ef27
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-02-11 13:22:58 +0100

    Fix imports in .well-known routes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import autoAdapter from '@sveltejs/adapter-auto';
 import nodeAdapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: process.env.DOCKER ? nodeAdapter({ precompress: true }) : autoAdapter()
+    adapter: process.env.DOCKER ? nodeAdapter({ precompress: true }) : autoAdapter(),
+    typescript: {
+      config(config) {
+        // The default globs in .svelte-kit/tsconfig.json ignore paths starting with a dot
+        config.include.push('../src/routes/.well-known/**/*.ts');
+        return config;
+      }
+    }
   }
 };
-
-export default config;
