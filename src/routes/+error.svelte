949b1138ac9860219c7ae2dfbd500d73df254f47
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-02-20 23:27:37 +0100

    Set title on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,10 @@
   import { page } from '$app/stores';
 </script>
 
+<svelte:head>
+  <title>Error</title>
+</svelte:head>
+
 <div class="error">
   <h1>{$page.status}</h1>
   <p>{$page.error?.message}</p>
