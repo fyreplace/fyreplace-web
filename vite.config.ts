4814b0b268fb61712eec1dbb7e7e7978e71390ad
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-03-11 16:31:53 +0100

    Generate sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
   plugins: [sveltekit()],
   build: {
-    assetsInlineLimit: 0
+    assetsInlineLimit: 0,
+    sourcemap: true
   }
 } as Partial<UserConfig>);
