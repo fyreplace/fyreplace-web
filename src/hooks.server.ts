4a7f7a6e9e876a03842316cf10d8946f86a39d23
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-07-30 13:56:22 +0200

    Add Sentry release

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,7 @@ import { env } from '$env/dynamic/public';
 Sentry.init({
   dsn: env.PUBLIC_SENTRY_DSN,
   environment: env.PUBLIC_SENTRY_ENVIRONMENT,
+  release: process.env.npm_package_name + '@' + env.PUBLIC_SENTRY_RELEASE,
   ignoreTransactions: ['/health'],
   tracesSampleRate: 0.1
 });
