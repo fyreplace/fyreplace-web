7454024b1ef4d6d02565419e99c7c62158fb01ea
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-09-06 12:44:21 +0200

    Make requests idempotent

--- src/lib/openapi/index.ts
+++ src/lib/openapi/index.ts
@@ -2,11 +2,16 @@ import { dev } from '$app/environment';
 import urls from '$lib/data/urls.json';
 import { getStoredItem, setStoredItem } from '$lib/storage';
 import { DisplayableError, eventBus } from '$lib/events';
+import { sleep } from '$lib/utils';
 import {
 	Configuration,
 	FetchError,
 	ResponseError,
 	TokensEndpointApi,
+	type ErrorContext,
+	type FetchParams,
+	type Middleware,
+	type RequestContext,
 	type TokensEndpointApiInterface
 } from './generated';
 import FakeTokensEndpointApi from './fakes/tokens-endpoint';
@@ -24,6 +29,7 @@ export async function getTokensClient(): Promise<TokensEndpointApiInterface> {
 async function makeConfiguration() {
 	return new Configuration({
 		basePath: await getBaseUrl(),
+		middleware: [new RequestIdMiddleware()],
 		accessToken: getStoredItem('connection.token') ?? undefined
 	});
 }
@@ -62,6 +68,48 @@ export async function call(
 			eventBus.publish(new DisplayableError('errors.connection'));
 		} else {
 			eventBus.publish(new DisplayableError('errors.unknown'));
+		}
+	}
+}
+
+class RequestIdMiddleware implements Middleware {
+	private static readonly headerName = 'X-Request-Id';
+	private static attempts = new Map<string, number>();
+
+	pre(context: RequestContext): Promise<FetchParams | void> {
+		const headers = { ...context.init.headers } as Record<string, string>;
+
+		if (!headers[RequestIdMiddleware.headerName]) {
+			headers[RequestIdMiddleware.headerName] = Math.random().toString(36).substring(2);
+		}
+
+		context.init.headers = headers;
+		return Promise.resolve(context);
+	}
+
+	async onError(context: ErrorContext): Promise<Response | void> {
+		let promise: ReturnType<RequestIdMiddleware['onError']>;
+
+		if (context.response) {
+			promise = Promise.resolve(context.response);
+		}
+
+		const headers = context.init.headers as Record<string, string>;
+		const requestId = headers[RequestIdMiddleware.headerName]!;
+		const retryCount = RequestIdMiddleware.attempts.get(requestId) ?? 1;
+
+		if (retryCount < 3) {
+			RequestIdMiddleware.attempts.set(requestId, retryCount + 1);
+			await sleep(1000);
+			promise = context.fetch(context.url, context.init);
+		} else {
+			promise = Promise.reject(context.error);
+		}
+
+		try {
+			return await promise;
+		} finally {
+			RequestIdMiddleware.attempts.delete(requestId);
 		}
 	}
 }
